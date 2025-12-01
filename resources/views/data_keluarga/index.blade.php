ba523340f33c01d1e3636df3657efd61d3421a9b
seikerahilirsatu-cloud <seikerahilirsatu@gmail.com>
2025-11-30 18:58:26 -0800

    Update index.blade.php-add coulumn status at data_keluarga.index

--- resources/views/data_keluarga/index.blade.php
+++ resources/views/data_keluarga/index.blade.php
@@ -44,6 +44,13 @@
                         <div class="text-xs text-gray-600 mt-2">{{ Str::limit($f->alamat,60) }}</div>
                         <div class="text-xs text-gray-600 mt-1">Lingkungan: <span class="font-medium">{{ $f->lingkungan }}</span></div>
                         <div class="text-xs text-gray-600 mt-1">Tgl Entri: <span class="font-medium">{{ $f->created_at }}</span></div>
+                    @if (f->status_keluarga==1)
+                        <div class="text-xs text-gray-600 mt-1">Status: <span class="font-medium">Warga Domisili</span></div>
+                    @elseif (f->status_keluarga==2)
+                        <div class="text-xs text-gray-600 mt-1">Status: <span class="font-medium">Warga Luar Domisili</span></div>
+                    @else
+                        <div class="text-xs text-gray-600 mt-1">Status: <span class="font-medium">Warga Domisili Baru</span></div>
+                    @endif
                     </div>
 
                         <div class="text-right">
